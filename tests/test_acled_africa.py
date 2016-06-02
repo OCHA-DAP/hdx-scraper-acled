4bfc576f3a38e5f4fec46a1a59731d029871c665
Michael Rans <rans@email.com>
2016-06-02 14:37:23 +0200

    Do tests the py.test way

--- tests/test_acled_africa.py
+++ tests/test_acled_africa.py
@@ -4,25 +4,26 @@
 Unit tests for the WorldPop class.
 
 '''
-import unittest
 from datetime import datetime
 from os.path import join
 
+import pytest
 from hdx.configuration import Configuration
 
 from acled_africa import generate_dataset
 
 
-class TestAcledAfrica(unittest.TestCase):
+class TestAcledAfrica():
     '''
     Tests for url generation.
 
     '''
 
-    def setUp(self):
-        self.configuration = Configuration(hdx_key_file=join('fixtures', '.hdxkey'))
+    @pytest.fixture(scope="session")
+    def configuration(self):
+        return Configuration(hdx_key_file=join('fixtures', '.hdxkey'))
 
-    def test_generate_dataset(self):
+    def test_generate_dataset(self, configuration):
         today = datetime.strptime('01062016', '%d%m%Y').date()
         expected_dataset = {'name': 'acled-conflict-data-for-africa-realtime-2016',
                            'title': 'ACLED Conflict Data for Africa (Realtime - 2016)',
@@ -30,10 +31,10 @@ class TestAcledAfrica(unittest.TestCase):
                             'groups': 'country'
                             }
 
-        actual_dataset = generate_dataset(self.configuration, today, "country")
-        self.assertEquals(expected_dataset, actual_dataset)
+        actual_dataset = generate_dataset(configuration, today, "country")
+        assert expected_dataset == actual_dataset
 
-        base_url = self.configuration['base_url']
+        base_url = configuration['base_url']
 
         expected_resources = [{'description': 'ACLED-All-Africa-File_20160101-to-20160528.xlsx',
                                'name': 'ACLED-All-Africa-File_20160101-to-date.xlsx',
@@ -44,13 +45,13 @@ class TestAcledAfrica(unittest.TestCase):
                                   'name': 'ACLED-All-Africa-File_20160101-to-date_csv.zip',
                                   'url': '%s2016/06/ACLED-All-Africa-File_20160101-to-20160528_csv.zip' % base_url,
                                   'format': 'zipped csv'}]
-        self.assertEquals(expected_resources, actual_dataset.get_resources())
+        assert expected_resources == actual_dataset.get_resources()
 
         expected_gallery = []
-        self.assertEquals(expected_gallery, actual_dataset.get_gallery())
+        assert expected_gallery == actual_dataset.get_gallery()
 
-    def test_generate_countries(self):
+    def test_generate_countries(self, configuration):
         today = datetime.strptime('01062016', '%d%m%Y').date()
-        actual_result = generate_dataset(self.configuration, today)
+        actual_result = generate_dataset(configuration, today)
 
-        self.assertEquals(58, len(actual_result['groups']))
+        assert len(actual_result['groups']) == 58

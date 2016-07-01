60ea77e1f53b48b1cd21d558758fee03ddd1c82d
Michael Rans <rans@email.com>
2016-07-01 13:20:49 +0200

    Update to reflect API change

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ that register datasets in HDX.
 import logging
 from datetime import datetime
 
-from hdx.collector.scraperwiki import wrapper
+from hdx.facades.scraperwiki import facade
 
 from acled_africa import generate_dataset
 
@@ -19,11 +19,11 @@ logger = logging.getLogger(__name__)
 
 
 def main(configuration):
-    '''Wrapper'''
+    '''Generate dataset and create it in HDX'''
 
     dataset = generate_dataset(configuration, datetime.now())
     dataset.update_yaml()
     dataset.create_in_hdx()
 
 if __name__ == '__main__':
-    wrapper(main, hdx_site='prod')
+    facade(main, hdx_site='test')

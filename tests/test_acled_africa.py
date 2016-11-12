efb6c08f95fe86d9c4b278d7918d3e433498871d
Michael Rans <rans@email.com>
2016-11-12 12:59:18 +0100

    Fix tests

--- tests/test_acled_africa.py
+++ tests/test_acled_africa.py
@@ -24,10 +24,21 @@ class TestAcledAfrica():
             'name': 'acled-conflict-data-for-africa-realtime-2016',
             'title': 'ACLED Conflict Data for Africa (Realtime - 2016)',
             'dataset_date': '05/28/2016',
-            'groups': 'country'
+            'data_update_frequency': '7',
+            'tags': [{'name': 'conflict'}, {'name': 'political violence'}, {'name': 'protests'}, {'name': 'war'}],
+            'groups': [{'id': 'ago'}, {'id': 'bdi'}, {'id': 'ben'}, {'id': 'bfa'}, {'id': 'bwa'}, {'id': 'caf'},
+                       {'id': 'civ'}, {'id': 'cmr'}, {'id': 'cod'}, {'id': 'cog'}, {'id': 'com'}, {'id': 'cpv'},
+                       {'id': 'dji'}, {'id': 'dza'}, {'id': 'egy'}, {'id': 'eri'}, {'id': 'esh'}, {'id': 'eth'},
+                       {'id': 'gab'}, {'id': 'gha'}, {'id': 'gin'}, {'id': 'gmb'}, {'id': 'gnb'}, {'id': 'gnq'},
+                       {'id': 'ken'}, {'id': 'lbr'}, {'id': 'lby'}, {'id': 'lso'}, {'id': 'mar'}, {'id': 'mdg'},
+                       {'id': 'mli'}, {'id': 'moz'}, {'id': 'mrt'}, {'id': 'mus'}, {'id': 'mwi'}, {'id': 'myt'},
+                       {'id': 'nam'}, {'id': 'ner'}, {'id': 'nga'}, {'id': 'reu'}, {'id': 'rwa'}, {'id': 'sdn'},
+                       {'id': 'sen'}, {'id': 'shn'}, {'id': 'sle'}, {'id': 'som'}, {'id': 'ssd'}, {'id': 'stp'},
+                       {'id': 'swz'}, {'id': 'syc'}, {'id': 'tcd'}, {'id': 'tgo'}, {'id': 'tun'}, {'id': 'tza'},
+                       {'id': 'uga'}, {'id': 'zaf'}, {'id': 'zmb'}, {'id': 'zwe'}],
         }
 
-        actual_dataset = generate_dataset(configuration, today, "country")
+        actual_dataset = generate_dataset(configuration, today)
         assert expected_dataset == actual_dataset
 
         base_url = configuration['base_url']

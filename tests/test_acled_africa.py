f7d5b587d52661200ca860659dabb764972b0918
Michael Rans <rans@email.com>
2017-02-21 12:22:52 +0100

    Fix test

--- tests/test_acled_africa.py
+++ tests/test_acled_africa.py
@@ -16,7 +16,8 @@ from acled_africa import generate_dataset
 class TestAcledAfrica():
     @pytest.fixture(scope='function')
     def configuration(self):
-        Configuration.create(hdx_key_file=join('fixtures', '.hdxkey'))
+        Configuration.create(hdx_key_file=join('fixtures', '.hdxkey'),
+                             project_config_yaml=join('config', 'project_configuration.yml'))
 
     def test_generate_dataset(self, configuration):
         today = datetime.strptime('01062016', '%d%m%Y').date()

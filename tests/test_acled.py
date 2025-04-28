cebf2ed89e1625529097a1970218fc9bcbfd49b5
Briar <briar.j.mills@gmail.com>
2025-04-28 15:43:03 -0400

    Refactor to latest setup

--- tests/test_acled.py
+++ tests/test_acled.py
@@ -1,56 +1,18 @@
-import filecmp
 from os.path import join
 
-import pytest
-from hdx.api.configuration import Configuration
 from hdx.api.utilities.hdx_error_handler import HDXErrorHandler
-from hdx.data.dataset import Dataset
+from hdx.utilities.compare import assert_files_same
 from hdx.utilities.downloader import Download
 from hdx.utilities.path import temp_dir
 from hdx.utilities.retriever import Retrieve
-from hdx.utilities.useragent import UserAgent
 
 from hdx.scraper.acled.acled import Acled
 
 
 class TestAcled:
-    @pytest.fixture(scope="function")
-    def configuration(self, config_dir):
-        UserAgent.set_global("test")
-        Configuration._create(
-            hdx_read_only=True,
-            hdx_site="prod",
-            project_config_yaml=join(config_dir, "project_configuration.yaml"),
-        )
-        return Configuration.read()
-
-    @pytest.fixture(scope="function")
-    def read_dataset(self, monkeypatch):
-        def read_from_hdx(dataset_name):
-            return Dataset.load_from_json(
-                join(
-                    "tests",
-                    "fixtures",
-                    "input",
-                    f"dataset-{dataset_name}.json",
-                )
-            )
-
-        monkeypatch.setattr(Dataset, "read_from_hdx", staticmethod(read_from_hdx))
-
-    @pytest.fixture(scope="class")
-    def fixtures_dir(self):
-        return join("tests", "fixtures")
-
-    @pytest.fixture(scope="class")
-    def input_dir(self, fixtures_dir):
-        return join(fixtures_dir, "input")
-
-    @pytest.fixture(scope="class")
-    def config_dir(self, fixtures_dir):
-        return join("src", "hdx", "scraper", "acled", "config")
-
-    def test_acled(self, configuration, read_dataset, fixtures_dir, input_dir, config_dir):
+    def test_acled(
+        self, configuration, read_dataset, fixtures_dir, input_dir, config_dir
+    ):
         with HDXErrorHandler() as error_handler:
             with temp_dir(
                 "TestAcled",
@@ -74,7 +36,9 @@ class TestAcled:
                     assert len(acled.data[2025]) == 14373
 
                     dataset = acled.generate_dataset()
-                    dataset.update_from_yaml(path=join(config_dir, "hdx_dataset_static.yaml"))
+                    dataset.update_from_yaml(
+                        path=join(config_dir, "hdx_dataset_static.yaml")
+                    )
                     assert dataset == {
                         "caveats": "",
                         "data_update_frequency": 7,
@@ -114,7 +78,11 @@ class TestAcled:
                             {
                                 "name": "conflict-violence",
                                 "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1",
-                            }
+                            },
+                            {
+                                "name": "hxl",
+                                "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1",
+                            },
                         ],
                         "title": "HDX HAPI - Coordination & Context: Conflict Events",
                     }
@@ -132,7 +100,7 @@ class TestAcled:
                         "url_type": "upload",
                     }
 
-                    assert filecmp.cmp(
+                    assert_files_same(
                         join(temp_folder, "hdx_hapi_conflict_event_global_2025.csv"),
                         join(fixtures_dir, "hdx_hapi_conflict_event_global_2025.csv"),
                     )
